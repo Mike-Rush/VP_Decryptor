408f3ffd3024ca558bcbe93f06a3bbf810e1a2c9
Mike-Rush <dddd1d23@163.com>
2021-02-21 20:29:58 +0800

    modify do_all.py

--- do_all.py
+++ do_all.py
@@ -1,5 +1,9 @@
-import sys,os
+import sys, os
 import argparse as AP
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_v1_5
+import P1735Parser
+
 def parse_args(args):
 	argParser = AP.ArgumentParser(description = "Decrypt SubDIR VP Files")
 	argParser.add_argument("-keyname",
@@ -40,6 +44,19 @@ def parse_encrypted_file(fd):
 		parser.feed(line)
 	return parser
 
+def rsa_decrypt(fd, data):
+	#print(fd.read())
+	fd.seek(0,0)
+	rsakey = RSA.importKey(fd.read())
+	cipher = PKCS1_v1_5.new(rsakey)
+	return cipher.decrypt(data, None)
+
+def aes128_cbc_decrypt(key, data):
+	unpad = lambda s : s[:-ord(s[len(s)-1:])]
+	iv = data[:16]
+	cipher = AES.new(key, AES.MODE_CBC, iv)
+	return unpad(cipher.decrypt(data[16:]))
+
 if __name__ == "__main__":
 	args = parse_args(sys.argv[1:])
 	if args.keyname == None:
@@ -57,6 +74,7 @@ if __name__ == "__main__":
 	#		print("    %s" % kn)
 	#	sys.exit(1)
 	#print(args.indir[0])
+	cnt=0
 	for root, dirs, files in os.walk(args.indir[0], topdown=True):
 		for name in dirs:
 			#print(os.path.join(root, name).replace(args.indir[0],args.outdir[0]))
@@ -64,5 +82,33 @@ if __name__ == "__main__":
 			os.mkdir(os.path.join(root, name).replace(args.indir[0],args.outdir[0]))		
 		for name in files:
 			if (os.path.splitext(name)[1] == '.vp'):
-				print(os.path.join(root, name))
+				cnt=cnt+1
+				infile_name=os.path.join(root, name)
+				print("Processing "+infile_name)
+				fdin=open(infile_name,"r")
+				try:
+					edata = parse_encrypted_file(fdin)
+				except:
+					print('Parse File Error')
+					continue
+				try:
+					esession_key = edata.session_keys[keyname]
+				except KeyError:
+					print("No such key: %s" % keyname)
+					print("Need any one of these:")
+					for kn in edata.session_keys.keys():
+						print("    %s" % kn)
+					continue
+					#sys.exit(1)
+				print(args.keyfile[0])
+				session_key = rsa_decrypt(args.keyfile[0], esession_key)
+				session_key_pos=session_key.find(b'session_keyx')+13
+				session_key_true=session_key[session_key_pos:session_key_pos+32]
+				str_tmp=bytes.decode(session_key_true)
+				session_key_bytes=bytes.fromhex(str_tmp)
+				decrypted_data = aes128_cbc_decrypt(session_key_bytes, edata.encrypted_data)
+				fdout=open(infile_name.replace(args.indir[0],args.outdir[0]),"wb")
+				#fdout.write(bytes.decode(decrypted_data))
+				fd
+
 
